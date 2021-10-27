f3906bb76d6f306ee03aee0b60de492217741127
wendallR <wendallar125@gmail.com>
2021-10-27 18:59:11 -0400

    Rename ArtistParser.java

---
+++ src/main/java/model/SearchResultParser.java
@@ -0,0 +1,20 @@
+package model;
+
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+
+public class SearchResultParser {
+
+    private JSONArray searchResultArray;
+
+    public SearchResultParser(JSONArray searchResultArray) {
+        this.searchResultArray = searchResultArray;
+    }
+
+    public String parseArtistInfo(String itemToBeParsed) {
+        JSONArray arrayContents = JsonPath.read(searchResultArray, String.format("$..%s",itemToBeParsed));
+        return arrayContents.get(0).toString();
+    }
+
+
+}
