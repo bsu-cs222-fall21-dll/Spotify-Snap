e3d5dd8a7bf837c4671471c54b9f0cc0024929c8
wendallR <wendallar125@gmail.com>
2021-10-29 17:52:51 -0400

    Make Album Class to store data from album search queries

---
+++ src/main/java/model/Album.java
@@ -0,0 +1,28 @@
+package model;
+
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+
+public class Album {
+
+    private final String name;
+    private final String id;
+    private final String uri;
+    private final String date;
+    private final String totalTracks;
+
+    public Album(JSONArray inputArray) {
+        SearchResultParser searchResultParser = new SearchResultParser(inputArray);
+        this.name = searchResultParser.parseArtistInfo("name");
+        this.id = searchResultParser.parseArtistInfo("id");
+        this.uri = searchResultParser.parseArtistInfo("uri");
+        this.date = searchResultParser.parseArtistInfo("release_date");
+        this.totalTracks = searchResultParser.parseArtistInfo("total_tracks");
+    }
+
+    public JSONArray readAlbumInfoAsJson() {
+        String artistArrayAsString = String.format("{\"album\": [{\"name\": \"%s\", \"id\": \"%s\",\"uri\": "
+                + "\"%s\",\"date\": \"%s\" ,\"total_tracks\": \"%s\"}]}",name,id,uri,date,totalTracks);
+        return JsonPath.read(artistArrayAsString,"$.*");
+    }
+}
