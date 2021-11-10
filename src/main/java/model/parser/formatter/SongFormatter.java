21f94bb15888b4c214f84ca4a7f8de638c805973
wendallR <wendallar125@gmail.com>
2021-11-10 18:50:50 -0500

    Add song formatter class

---
+++ src/main/java/model/parser/formatter/SongFormatter.java
@@ -0,0 +1,26 @@
+package model.parser.formatter;
+
+import model.parser.Album;
+import model.parser.Parser;
+import model.parser.Song;
+
+public class SongFormatter extends Parser implements Formatter{
+
+
+    public SongFormatter(Song song) {
+        super(song.readSongInfoAsJson());
+    }
+
+    public String format() {
+        String name = parseInfo("name");
+        String id = parseInfo("id");
+        String uri = parseInfo("uri");
+
+        String duration_ms = parseInfo("duration_ms");
+        String track_number = parseInfo("track_number");
+        String explicit = parseInfo("explicit");
+
+        return String.format("\nSong Name: %s\nSong Id: %s\nUri: %s\nDuration in MS: %s\nExplicit: %s\nTrack Number: %s\n",
+                name,id,uri,duration_ms,explicit,track_number);
+    }
+}
