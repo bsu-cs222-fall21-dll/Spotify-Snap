0934b1fd9c5f705c42c78189647db3b0f0fa494d
ggarcia23 <gagarcia3@bsu.edu>
2021-11-16 18:52:06 -0500

    added mock up GUI to project

---
+++ src/main/java/view/gui/spotifySnapInterface.java
@@ -0,0 +1,36 @@
+package view.gui;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class spotifySnapInterface extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox parent = new VBox();
+        parent.setAlignment(Pos.TOP_CENTER);
+        parent.setSpacing(8);
+        parent.getChildren().add(new Label("Spotify-Snap"));
+
+        HBox urlArea = new HBox(new Label("Enter Artist: "));
+        urlArea.setAlignment(Pos.TOP_CENTER);
+
+        TextField textField = new TextField();
+        urlArea.getChildren().add(textField);
+        parent.getChildren().add(urlArea);
+
+        Button button = new Button("Search Artist");
+        button.setOnAction(event -> System.out.println("I would like to see info on " + textField.getText()));
+
+        parent.getChildren().add(button);
+
+        primaryStage.setScene(new Scene(parent, 370, 425));
+        primaryStage.show();
+    }
+}
