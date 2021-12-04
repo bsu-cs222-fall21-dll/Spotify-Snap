4dfe590969c85494eaebb59a3aa9ac1fec2fe2be
Merghani Hag Elnour <mthagelnour@bsu.edu>
2021-12-04 17:10:43 -0500

    Add SceneOneController.validateFieldIsEmpty checks if userInput field is empty in the GUI

--- src/main/java/SceneOneController.java
+++ src/main/java/SceneOneController.java
@@ -4,11 +4,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import model.parser.hashtable.AlbumHashTable;
+import model.parser.hashtable.AlbumHashTableBuilder;
+import model.requests.ArtistAlbums;
+import model.requests.ArtistID;
+import model.type.Artist;
+import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -30,10 +37,29 @@ public class SceneOneController {
 
 
     public void searchButtonHandler(ActionEvent event) throws IOException {
+        if (userInputField.getText().isEmpty()) {
+            validateFieldIsEmpty();
+        } else {
+
+            switchScene(event);
+        }
+
+    }
+
+    public void switchScene(ActionEvent event) throws IOException {
+
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SecondaryScene.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void validateFieldIsEmpty(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Warning");
+        alert.setHeaderText("Empty Field");
+        alert.setContentText("Enter search");
+        alert.showAndWait();
     }
 }
